bc85aa0dd769a9cbaf9788eb53e77db142c845f2
sahil <latthersahil@gmail.com>
2019-10-12 10:19:17 -0400

    xyz

--- app/src/main/java/com/example/mad1897126/Main2Activity.java
+++ app/src/main/java/com/example/mad1897126/Main2Activity.java
@@ -2,13 +2,40 @@ package com.example.mad1897126;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity
+{
+    Button b4;
+    int a1,b1,c1;
+    TextView sum,product;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        b4 = findViewById(R.id.button3);
+        sum =  findViewById(R.id.t1);
+        product = findViewById(R.id.t2);
+        Intent intent = getIntent();
+        a1 = intent.getIntExtra("a1",0);
+        b1 = intent.getIntExtra("b1",0);
+        c1 = intent.getIntExtra("c1",0);
+         sum.setText("Sum - "+(a1+b1+c1));
+         product.setText("Product - "+(a1*b1*c1));
+         b4.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent i = new Intent(Main2Activity.this,MainActivity.class);
+                 startActivity(i);
+             }
+         });
+
+
+
     }
 }
--- app/src/main/java/com/example/mad1897126/MainActivity.java
+++ app/src/main/java/com/example/mad1897126/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.mad1897126;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText ed1,ed2,ed5;
+    Button b1;
+    int a,b,c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1 = findViewById(R.id.button2);
+        ed1 = findViewById(R.id.ed1);
+        ed2 = findViewById(R.id.ed2);
+        ed5 = findViewById(R.id.ed5);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                a = Integer.parseInt(ed1.getText().toString());
+                b = Integer.parseInt(ed2.getText().toString());
+                c = Integer.parseInt(ed5.getText().toString());
+                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                intent.putExtra("a1",a);
+                intent.putExtra("b1",b);
+                intent.putExtra("c1",c);
+
+                startActivity(intent);
+
+
+
+
+            }
+        });
     }
 }
